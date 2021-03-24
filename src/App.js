1b925d7b20b5cc3be622659f355739f2e92ebea0
dshearwood <d.algorhythmic@gmail.com>
2021-03-24 15:30:24 -0400

    Created

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
